ca054837391d90c676111b2a08902ab9d944554b
Weerawat Tarujettanarom <weerawat.36@gmail.com>
2019-11-29 17:13:27 +0700

    Add project files.

---
+++ pbi_prepare/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace pbi_prepare
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDestination = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSource = new System.Windows.Forms.Button();
+            this.btnDestination = new System.Windows.Forms.Button();
+            this.btnExtractFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(136, 16);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.ReadOnly = true;
+            this.txtSource.Size = new System.Drawing.Size(251, 20);
+            this.txtSource.TabIndex = 0;
+            this.txtSource.TextChanged += new System.EventHandler(this.txtSource_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Source pbix file";
+            // 
+            // txtDestination
+            // 
+            this.txtDestination.Location = new System.Drawing.Point(136, 42);
+            this.txtDestination.Name = "txtDestination";
+            this.txtDestination.ReadOnly = true;
+            this.txtDestination.Size = new System.Drawing.Size(251, 20);
+            this.txtDestination.TabIndex = 0;
+            this.txtDestination.TextChanged += new System.EventHandler(this.txtDestination_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Destination pbix file";
+            // 
+            // btnSource
+            // 
+            this.btnSource.Location = new System.Drawing.Point(393, 14);
+            this.btnSource.Name = "btnSource";
+            this.btnSource.Size = new System.Drawing.Size(30, 23);
+            this.btnSource.TabIndex = 2;
+            this.btnSource.Text = "...";
+            this.btnSource.UseVisualStyleBackColor = true;
+            this.btnSource.Click += new System.EventHandler(this.btnSource_Click);
+            // 
+            // btnDestination
+            // 
+            this.btnDestination.Location = new System.Drawing.Point(393, 40);
+            this.btnDestination.Name = "btnDestination";
+            this.btnDestination.Size = new System.Drawing.Size(30, 23);
+            this.btnDestination.TabIndex = 2;
+            this.btnDestination.Text = "...";
+            this.btnDestination.UseVisualStyleBackColor = true;
+            this.btnDestination.Click += new System.EventHandler(this.btnDestination_Click);
+            // 
+            // btnExtractFile
+            // 
+            this.btnExtractFile.Location = new System.Drawing.Point(193, 86);
+            this.btnExtractFile.Name = "btnExtractFile";
+            this.btnExtractFile.Size = new System.Drawing.Size(75, 23);
+            this.btnExtractFile.TabIndex = 3;
+            this.btnExtractFile.Text = "Extract File";
+            this.btnExtractFile.UseVisualStyleBackColor = true;
+            this.btnExtractFile.Click += new System.EventHandler(this.btnExtractFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 134);
+            this.Controls.Add(this.btnExtractFile);
+            this.Controls.Add(this.btnDestination);
+            this.Controls.Add(this.btnSource);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDestination);
+            this.Controls.Add(this.txtSource);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDestination;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSource;
+        private System.Windows.Forms.Button btnDestination;
+        private System.Windows.Forms.Button btnExtractFile;
+    }
+}
+
---
+++ pbi_prepare/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ICSharpCode.SharpZipLib.Core;
+using ICSharpCode.SharpZipLib.Zip;
+
+namespace pbi_prepare
+{
+    public partial class Form1 : Form
+    {
+        private string source_file = null;
+        private string destination_folder = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtSource_TextChanged(object sender, EventArgs e)
+        {
+            this.source_file = ((TextBox)sender).Text;
+        }
+
+        private void txtDestination_TextChanged(object sender, EventArgs e)
+        {
+            this.destination_folder = ((TextBox)sender).Text;
+        }
+
+        private void btnSource_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                this.txtSource.Text = ofd.FileName;
+            }
+        }
+
+        private void btnDestination_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            
+            if(fbd.ShowDialog() == DialogResult.OK)
+            {
+                this.txtDestination.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void btnExtractFile_Click(object sender, EventArgs e)
+        {
+            if (this.ExtractZipFile(this.source_file, "", this.destination_folder))
+            {
+                MessageBox.Show("Success");
+            }
+            else
+            {
+                MessageBox.Show("Failed");
+            }
+        }
+        public bool ExtractZipFile(string archivePath, string password, string outFolder)
+        {
+            try
+            {
+                using (Stream fsInput = File.OpenRead(archivePath))
+                {
+                    using (ZipFile zf = new ZipFile(fsInput))
+                    {
+                        if (!String.IsNullOrEmpty(password))
+                        {
+                            // AES encrypted entries are handled automatically
+                            zf.Password = password;
+                        }
+
+                        foreach (ZipEntry zipEntry in zf)
+                        {
+                            if (!zipEntry.IsFile)
+                            {
+                                // Ignore directories
+                                continue;
+                            }
+                            String entryFileName = zipEntry.Name;
+                            // to remove the folder from the entry:
+                            //entryFileName = Path.GetFileName(entryFileName);
+                            // Optionally match entrynames against a selection list here
+                            // to skip as desired.
+                            // The unpacked length is available in the zipEntry.Size property.
+
+                            // Manipulate the output filename here as desired.
+                            var fullZipToPath = Path.Combine(outFolder, entryFileName);
+                            var directoryName = Path.GetDirectoryName(fullZipToPath);
+                            if (directoryName.Length > 0)
+                            {
+                                Directory.CreateDirectory(directoryName);
+                            }
+
+                            // 4K is optimum
+                            var buffer = new byte[4096];
+
+                            // Unzip file in buffered chunks. This is just as fast as unpacking
+                            // to a buffer the full size of the file, but does not waste memory.
+                            // The "using" will close the stream even if an exception occurs.
+                            using (var zipStream = zf.GetInputStream(zipEntry))
+                            using (Stream fsOutput = File.Create(fullZipToPath))
+                            {
+                                StreamUtils.Copy(zipStream, fsOutput, buffer);
+                            }
+                        }
+                    }
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
+    }
+}
