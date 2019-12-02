fb4cbcdf4ee37c91010ec75d1c9c22d25910f602
Weerawat Tarujettanarom <weerawat.36@gmail.com>
2019-12-02 17:08:34 +0700

    Compress/Uncompress completed.

--- pbi_prepare/Form1.Designer.cs
+++ pbi_prepare/Form1.Designer.cs
@@ -30,11 +30,16 @@
         {
             this.txtSource = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.txtDestination = new System.Windows.Forms.TextBox();
+            this.txtExtractTo = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.btnSource = new System.Windows.Forms.Button();
-            this.btnDestination = new System.Windows.Forms.Button();
+            this.btnExtractTo = new System.Windows.Forms.Button();
             this.btnExtractFile = new System.Windows.Forms.Button();
+            this.btnCompressFile = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDestination = new System.Windows.Forms.TextBox();
+            this.btnDestination = new System.Windows.Forms.Button();
+            this.btnCreateJsonFile = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtSource
@@ -55,23 +60,23 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Source pbix file";
             // 
-            // txtDestination
+            // txtExtractTo
             // 
-            this.txtDestination.Location = new System.Drawing.Point(136, 42);
-            this.txtDestination.Name = "txtDestination";
-            this.txtDestination.ReadOnly = true;
-            this.txtDestination.Size = new System.Drawing.Size(251, 20);
-            this.txtDestination.TabIndex = 0;
-            this.txtDestination.TextChanged += new System.EventHandler(this.txtDestination_TextChanged);
+            this.txtExtractTo.Location = new System.Drawing.Point(136, 42);
+            this.txtExtractTo.Name = "txtExtractTo";
+            this.txtExtractTo.ReadOnly = true;
+            this.txtExtractTo.Size = new System.Drawing.Size(251, 20);
+            this.txtExtractTo.TabIndex = 0;
+            this.txtExtractTo.TextChanged += new System.EventHandler(this.txtExtractTo_TextChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(30, 45);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.Size = new System.Drawing.Size(52, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Destination pbix file";
+            this.label2.Text = "Extract to";
             // 
             // btnSource
             // 
@@ -83,37 +88,92 @@
             this.btnSource.UseVisualStyleBackColor = true;
             this.btnSource.Click += new System.EventHandler(this.btnSource_Click);
             // 
-            // btnDestination
+            // btnExtractTo
             // 
-            this.btnDestination.Location = new System.Drawing.Point(393, 40);
-            this.btnDestination.Name = "btnDestination";
-            this.btnDestination.Size = new System.Drawing.Size(30, 23);
-            this.btnDestination.TabIndex = 2;
-            this.btnDestination.Text = "...";
-            this.btnDestination.UseVisualStyleBackColor = true;
-            this.btnDestination.Click += new System.EventHandler(this.btnDestination_Click);
+            this.btnExtractTo.Location = new System.Drawing.Point(393, 40);
+            this.btnExtractTo.Name = "btnExtractTo";
+            this.btnExtractTo.Size = new System.Drawing.Size(30, 23);
+            this.btnExtractTo.TabIndex = 2;
+            this.btnExtractTo.Text = "...";
+            this.btnExtractTo.UseVisualStyleBackColor = true;
+            this.btnExtractTo.Click += new System.EventHandler(this.btnExtractTo_Click);
             // 
             // btnExtractFile
             // 
-            this.btnExtractFile.Location = new System.Drawing.Point(193, 86);
+            this.btnExtractFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExtractFile.Location = new System.Drawing.Point(222, 119);
             this.btnExtractFile.Name = "btnExtractFile";
-            this.btnExtractFile.Size = new System.Drawing.Size(75, 23);
+            this.btnExtractFile.Size = new System.Drawing.Size(111, 23);
             this.btnExtractFile.TabIndex = 3;
             this.btnExtractFile.Text = "Extract File";
             this.btnExtractFile.UseVisualStyleBackColor = true;
             this.btnExtractFile.Click += new System.EventHandler(this.btnExtractFile_Click);
             // 
+            // btnCompressFile
+            // 
+            this.btnCompressFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCompressFile.Location = new System.Drawing.Point(339, 119);
+            this.btnCompressFile.Name = "btnCompressFile";
+            this.btnCompressFile.Size = new System.Drawing.Size(111, 23);
+            this.btnCompressFile.TabIndex = 4;
+            this.btnCompressFile.Text = "Compress to Zip";
+            this.btnCompressFile.UseVisualStyleBackColor = true;
+            this.btnCompressFile.Click += new System.EventHandler(this.btnCompressFile_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Destination pbix file";
+            // 
+            // txtDestination
+            // 
+            this.txtDestination.Location = new System.Drawing.Point(136, 69);
+            this.txtDestination.Name = "txtDestination";
+            this.txtDestination.Size = new System.Drawing.Size(251, 20);
+            this.txtDestination.TabIndex = 5;
+            this.txtDestination.TextChanged += new System.EventHandler(this.txtDestination_TextChanged);
+            // 
+            // btnDestination
+            // 
+            this.btnDestination.Location = new System.Drawing.Point(393, 68);
+            this.btnDestination.Name = "btnDestination";
+            this.btnDestination.Size = new System.Drawing.Size(30, 23);
+            this.btnDestination.TabIndex = 6;
+            this.btnDestination.Text = "...";
+            this.btnDestination.UseVisualStyleBackColor = true;
+            this.btnDestination.Click += new System.EventHandler(this.btnDestination_Click);
+            // 
+            // btnCreateJsonFile
+            // 
+            this.btnCreateJsonFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnCreateJsonFile.Location = new System.Drawing.Point(12, 119);
+            this.btnCreateJsonFile.Name = "btnCreateJsonFile";
+            this.btnCreateJsonFile.Size = new System.Drawing.Size(109, 23);
+            this.btnCreateJsonFile.TabIndex = 7;
+            this.btnCreateJsonFile.Text = "Create Json File";
+            this.btnCreateJsonFile.UseVisualStyleBackColor = true;
+            this.btnCreateJsonFile.Click += new System.EventHandler(this.btnCreateJsonFile_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(462, 134);
-            this.Controls.Add(this.btnExtractFile);
+            this.ClientSize = new System.Drawing.Size(462, 154);
+            this.Controls.Add(this.btnCreateJsonFile);
             this.Controls.Add(this.btnDestination);
+            this.Controls.Add(this.txtDestination);
+            this.Controls.Add(this.btnCompressFile);
+            this.Controls.Add(this.btnExtractFile);
+            this.Controls.Add(this.btnExtractTo);
             this.Controls.Add(this.btnSource);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtDestination);
+            this.Controls.Add(this.txtExtractTo);
             this.Controls.Add(this.txtSource);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "Form1";
@@ -127,11 +187,16 @@
 
         private System.Windows.Forms.TextBox txtSource;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtDestination;
+        private System.Windows.Forms.TextBox txtExtractTo;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnSource;
-        private System.Windows.Forms.Button btnDestination;
+        private System.Windows.Forms.Button btnExtractTo;
         private System.Windows.Forms.Button btnExtractFile;
+        private System.Windows.Forms.Button btnCompressFile;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDestination;
+        private System.Windows.Forms.Button btnDestination;
+        private System.Windows.Forms.Button btnCreateJsonFile;
     }
 }
 
--- pbi_prepare/Form1.cs
+++ pbi_prepare/Form1.cs
@@ -15,7 +15,8 @@ namespace pbi_prepare
     public partial class Form1 : Form
     {
         private string source_file = null;
-        private string destination_folder = null;
+        private string extract_to_folder = null;
+        private string destination_file = null;
 
         public Form1()
         {
@@ -27,9 +28,9 @@ namespace pbi_prepare
             this.source_file = ((TextBox)sender).Text;
         }
 
-        private void txtDestination_TextChanged(object sender, EventArgs e)
+        private void txtExtractTo_TextChanged(object sender, EventArgs e)
         {
-            this.destination_folder = ((TextBox)sender).Text;
+            this.extract_to_folder = ((TextBox)sender).Text;
         }
 
         private void btnSource_Click(object sender, EventArgs e)
@@ -42,19 +43,19 @@ namespace pbi_prepare
             }
         }
 
-        private void btnDestination_Click(object sender, EventArgs e)
+        private void btnExtractTo_Click(object sender, EventArgs e)
         {
             FolderBrowserDialog fbd = new FolderBrowserDialog();
             
             if(fbd.ShowDialog() == DialogResult.OK)
             {
-                this.txtDestination.Text = fbd.SelectedPath;
+                this.txtExtractTo.Text = fbd.SelectedPath;
             }
         }
 
         private void btnExtractFile_Click(object sender, EventArgs e)
         {
-            if (this.ExtractZipFile(this.source_file, "", this.destination_folder))
+            if (this.ExtractZipFile(this.source_file, "", this.extract_to_folder))
             {
                 MessageBox.Show("Success");
             }
@@ -123,5 +124,109 @@ namespace pbi_prepare
             }
         }
 
+        private void btnDestination_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.FileName = "new_pbix.pbix";
+            if(sfd.ShowDialog() == DialogResult.OK)
+            {
+                this.txtDestination.Text = sfd.FileName;
+            }
+        }
+
+        private void txtDestination_TextChanged(object sender, EventArgs e)
+        {
+            this.destination_file = ((TextBox)sender).Text;
+        }
+
+        private void btnCompressFile_Click(object sender, EventArgs e)
+        {
+            this.CreateSample(this.destination_file, "", this.extract_to_folder);
+        }
+
+        // Compresses the files in the nominated folder, and creates a zip file 
+        // on disk named as outPathname.
+        public void CreateSample(string outPathname, string password, string folderName)
+        {
+
+            using (FileStream fsOut = File.Create(outPathname))
+            using (var zipStream = new ZipOutputStream(fsOut))
+            {
+
+                //0-9, 9 being the highest level of compression
+                zipStream.SetLevel(3);
+
+                // optional. Null is the same as not setting. Required if using AES.
+                zipStream.Password = password;
+
+                // This setting will strip the leading part of the folder path in the entries, 
+                // to make the entries relative to the starting folder.
+                // To include the full path for each entry up to the drive root, assign to 0.
+                int folderOffset = folderName.Length + (folderName.EndsWith("\\") ? 0 : 1);
+
+                CompressFolder(folderName, zipStream, folderOffset);
+
+            }
+
+        }
+
+        public void CompressFolder(string path, ZipOutputStream zipStream, int folderOffset)
+        {
+            var files = Directory.GetFiles(path);
+
+            foreach (var filename in files)
+            {
+
+                var fi = new FileInfo(filename);
+
+                // Make the name in zip based on the folder
+                var entryName = filename.Substring(folderOffset);
+
+                // Remove drive from name and fixe slash direction
+                entryName = ZipEntry.CleanName(entryName);
+
+                var newEntry = new ZipEntry(entryName);
+
+                // Note the zip format stores 2 second granularity
+                newEntry.DateTime = fi.LastWriteTime;
+
+                // Specifying the AESKeySize triggers AES encryption. 
+                // Allowable values are 0 (off), 128 or 256.
+                // A password on the ZipOutputStream is required if using AES.
+                //   newEntry.AESKeySize = 256;
+
+                // To permit the zip to be unpacked by built-in extractor in WinXP and Server2003,
+                // WinZip 8, Java, and other older code, you need to do one of the following: 
+                // Specify UseZip64.Off, or set the Size.
+                // If the file may be bigger than 4GB, or you do not need WinXP built-in compatibility, 
+                // you do not need either, but the zip will be in Zip64 format which
+                // not all utilities can understand.
+                //   zipStream.UseZip64 = UseZip64.Off;
+                newEntry.Size = fi.Length;
+
+                zipStream.PutNextEntry(newEntry);
+
+                // Zip the file in buffered chunks
+                // the "using" will close the stream even if an exception occurs
+                var buffer = new byte[4096];
+                using (FileStream fsInput = File.OpenRead(filename))
+                {
+                    StreamUtils.Copy(fsInput, zipStream, buffer);
+                }
+                zipStream.CloseEntry();
+            }
+
+            // Recursively call CompressFolder on all folders in path
+            var folders = Directory.GetDirectories(path);
+            foreach (var folder in folders)
+            {
+                CompressFolder(folder, zipStream, folderOffset);
+            }
+        }
+
+        private void btnCreateJsonFile_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
